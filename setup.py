a138e9d3d67b84990415d9d806ea099332cef60b
Andrew Kos <akos123@gmail.com>
2015-11-13 11:24:14 -0600

    Version up to 1.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 # ---------------------------------------
 # set this sucker!!
 
-VERSION = '1.0.1'
+VERSION = '1.0.2'
 # ---------------------------------------
 
 name = "drf-nested-serializers"
